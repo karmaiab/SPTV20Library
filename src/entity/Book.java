dff58a1d8d5f1eaccb88d87a1d56b585b5b8d3b2
sergeyts <sergeysill@gmail.com>
2021-10-12 15:12:41 +0300

    second commit

---
+++ src/entity/Book.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author User
+ */
+public class Book {
+    private String BookName;
+    private Author[] authors;
+    private int releaseYear;
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String BookName) {
+        this.BookName = BookName;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setAuthors() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/myclasses/App.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+import entity.Book;
+import entity.Author;
+
+/**
+ *
+ * @author User
+ */
+public class App {
+    public void run(){
+       // System.out.println("Hello");
+        Book book = new Book();
+        book.setBookName("Voina i mir");
+        book.setReleaseYear(2010);
+        Author[] authors = new Author[1];
+        Author author = new Author();
+        author.setFirstName("Lev");
+        author.setLastName("Tolstoy");
+        author.setBirthYear(1828);
+        authors[0] = author;
+        book.setAuthors(authors);
+        System.out.printf("Создана книга: %s, автор: %s %s, год издания: %d%n", 
+                            book.getBookName(),
+                            book.getAuthors()[0].getFirstName(),
+                            book.getAuthors()[0].getLastName(),
+                            book.getReleaseYear()
+                );
+        
+    }
+}
